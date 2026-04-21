4dac6567c8b6cfa52df17711ae286e92cc7e50bf
HFWedding <itadmin@hfweddinghirecars.com>
2026-04-22 08:08:42 +1000

    20260422

--- app.py
+++ app.py
@@ -25,8 +25,6 @@ else:
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SESSION_COOKIE_SECURE'] = False
 
-print("IS_PROD:", IS_PROD)
-
 db = SQLAlchemy(app)
 
 class User(db.Model):
